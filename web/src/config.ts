36036ac1cbafef3a902b699844fae9916c7f2283
akikuno <akikuno@users.noreply.github.com>
2025-11-30 18:40:51 +0900

    Update Bio

--- web/src/config.ts
+++ web/src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "久野 朗広 (Akihiro Kuno)",
-	bio: "実験動物学 x バイオインフォマティクス",
+	bio: "🐁 x 💻️ x ⚕️",
 	links: [
 		{
 			name: "Researchmap",
